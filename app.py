792fe948ef665feba7bad5f25146b5850de86040
John Sattari <johnsattari@johns-air.lan>
2022-10-05 22:32:00 -0700

    completed bulk download functionality in bulk route

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from io import BytesIO, StringIO
 
 ALLOWED_EXTENSIONS = ["txt", "csv"]
 MAX_CONTENT_LENGTH = 524288
+FUNCTION_MAP = [tools.get_address_values, tools.get_xml, tools.get_zips]
 
 # app object
 app = Flask(__name__)
@@ -76,57 +77,56 @@ def upload_bulk_file():
     if buffer.seek(0, os.SEEK_END) > MAX_CONTENT_LENGTH:
         return render_template("too_large.html")
 
-    # read file into file buffer using csv reader
-    reader = csv.reader(buffer.getvalue().decode("UTF-8"))
+    try:
+        # read file into file buffer using csv reader
+        reader = csv.reader(buffer.getvalue().decode("UTF-8"))
 
-    # turn reader object into list
-    data = [value[0] for value in list(reader) if value]
+        # turn reader object into list
+        data = [value[0] for value in list(reader) if value]
 
-    # filter out random blanks
-    data = list(filter(None, data))
+        # filter out random blanks
+        data = list(filter(None, data))
 
-    # create string file buffer
-    csvfile = StringIO()
-    writer = csv.writer(csvfile)
+    except Exception as e:
+        response = f"Oh no! An error occured: {e}"
+        return render_template("bulk.html", response=response)
 
-    data_dict = {
-        str(index): [data[index: index + 5]]
-        for index in range(0, len(data), 5)
-    }
+    else:
+        # create string file buffer
+        csvfile = StringIO()
+        writer = csv.writer(csvfile)
 
-    for key, value in data_dict.items():
-        data_dict[key] = value + [tools.get_address_values(value[0])]
+        # create data structure to hold values for addresses
+        data_dict = {
+            str(index): [data[index: index + 5]]
+            for index in range(0, len(data), 5)
+        }
 
-    for key, value in data_dict.items():
-        data_dict[key] = value + [tools.get_xml(value[1])]
+        # map each function to data_dict variable
+        for func in FUNCTION_MAP:
+            for key, value in data_dict.items():
+                data_dict[key] = value + [func(value[-1])]
 
-    for key, value in data_dict.items():
-        data_dict[key] = value + [tools.get_zips(value[2])]
+        # parse values from data_dict into file buffer
+        for value in data_dict.values():
+            temp = 0
+            while temp < len(value[0]):
+                address = value[0][temp]
+                zip_code = value[3][temp]
+                writer.writerow([address, zip_code])
+                temp += 1
 
-    print(data_dict.get("0"))
+        # go to start of string file buffer
+        csvfile.seek(0)
 
-    # parse values from data_dict into file buffer
-    for value in data_dict.values():
-        temp = 0
-        while temp < len(value[0]):
-            x = value[0][temp]
-            y = value[1][temp]
-            z = value[2][temp]
-            q = value[3][temp]
-            writer.writerow([x, y, z, q])
-            temp += 1
-
-    # go to start of string file buffer
-    csvfile.seek(0)
-
-    # return buffer object as downloadable file
-    return Response(
-        csvfile.getvalue(),
-        mimetype="text/plain",
-        headers={
-            "Content-Disposition": "attachment;filename=results.csv"}), \
-        buffer.close(), \
-        csvfile.close()
+        # return buffer object as downloadable file
+        return Response(
+            csvfile.getvalue(),
+            mimetype="text/plain",
+            headers={
+                "Content-Disposition": "attachment;filename=results.csv"}), \
+            buffer.close(), \
+            csvfile.close()
 
 
 if __name__ == "__main__":
